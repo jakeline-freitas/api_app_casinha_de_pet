1e2d91d5a54e5763249b2619efd986949c37bb5d
jakeline-freitas <jakefreaitas@gmail.com>
2021-09-13 21:42:07 -0300

    accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.expressions import F
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True, blank=False)
+    phone = models.CharField(max_length=55)
+    first_name = None
+    last_name = None
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
--- casinhaDePet/settings.py
+++ casinhaDePet/settings.py
@@ -130,6 +130,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+AUTH_USER_MODEL = 'accounts.User'
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+    
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
